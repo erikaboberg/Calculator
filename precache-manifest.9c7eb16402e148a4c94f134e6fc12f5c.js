cc4d9b823cc86e69a600edda9494777a669972ba
Erika Boberg <erika.boberg91@gmail.com>
2019-02-13 22:35:42 +0100

    Updates

---
+++ precache-manifest.9c7eb16402e148a4c94f134e6fc12f5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5f5139b78fafdf93bd1",
+    "url": "/calculator/static/js/runtime~main.b5f5139b.js"
+  },
+  {
+    "revision": "c322ba620f27be9b9b97",
+    "url": "/calculator/static/js/main.c322ba62.chunk.js"
+  },
+  {
+    "revision": "1015926caf23046f2ba3",
+    "url": "/calculator/static/js/1.1015926c.chunk.js"
+  },
+  {
+    "revision": "c322ba620f27be9b9b97",
+    "url": "/calculator/static/css/main.cca3c069.chunk.css"
+  },
+  {
+    "revision": "4b7c886b3e9e8ef735f8a4a0334c66e5",
+    "url": "/calculator/index.html"
+  }
+];
